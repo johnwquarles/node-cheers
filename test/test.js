f8d01e9f63e83ff890588e60b4d0916cf913775b
johnwquarles <john.w.quarles@gmail.com>
2015-07-22 14:26:33 -0500

    changed from coveralls to codeclimate

--- test/test.js
+++ test/test.js
@@ -2,6 +2,9 @@ var should = require('chai').should();
 //var path = require('path');
 var cp = require('child_process');
 
+require(process.cwd() + '/cheers.js');
+require(process.cwd() + '/lib/help.js');
+
 describe('Mocha + Chai', function() {
   it('truthiness', function () {
     true.should.equal(true);
